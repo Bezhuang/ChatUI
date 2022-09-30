d7bb9b81d03f5064e765b122ce895e3290ccbe59
Bezhuang <bezhuang@outlook.com>
2022-09-30 17:31:13 +0800

    完成侧导航组件开发
    Icon组件、Badge组件、菜单项组件、侧导航组件

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { addDecorator } from "@storybook/react";
+import { addDecorator, addParameters } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
 import theme from "../src/theme";
 
-addDecorator((storyFn) => (
-  <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider >
-));
+import "story.css";
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
+export const parameters = {
+  options: {
+    showRoots: true,
+  },
+};
