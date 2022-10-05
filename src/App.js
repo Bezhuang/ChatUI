abaf001a61a23846b674f6ab2ab76c2db2538582
Bezhuang <bezhuang@outlook.com>
2022-10-05 23:03:04 +0800

    侧边栏消息列表组件
    完成

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import useColorSwtich from './useColorSwitch';
 function App() {
   const [color, handleButton1Click] = useColorSwtich();
   const [color2, handleButton2Click] = useColorSwtich("#0000ff", "#ff00ff");
-  return (
-    <div>
-      <Button width="120px" onClick={handleButton1Click} label="label">
-        <span>&gt;</span>
-      </Button>
-      <p style={{ color }}>这是一段文本</p>
-      <Button onClick={handleButton2Click} label="点我" />
-      <p style={{ color: color2 }}>这是第二段文本</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -1,10 +1,10 @@
 import { css } from "styled-components";
 
 export const circle = (color, size = "8px") => css`
-    width: ${size};
-    height: ${size};
-    border-radius: 50%;
-    background-color: ${color};
+  width: ${size};
+  height: ${size};
+  border-radius: 50%;
+  background-color: ${color};
 `;
 
 export const activeBar = ({ barWidth = "8px", shadowWidth = "20px" } = {}) =>
@@ -39,3 +39,10 @@ export const activeBar = ({ barWidth = "8px", shadowWidth = "20px" } = {}) =>
       opacity: 0.6;
     }
   `;
+
+export const card = (radius = "6px", padding = "20px 30px") => css`
+  padding: ${padding};
+  background: ${({ theme }) => theme.background};
+  box-shadow: 0px 18px 40px 0px rgba(0, 0, 0, 0.04);
+  border-radius: ${radius};
+`;
