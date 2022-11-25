a80453a5f8532c738ffb241f71834baba286f74f
Bezhuang <bezhuang@outlook.com>
2022-11-25 21:58:51 +0800

    add 设置页面&Filter列表

--- src/components/Input/input.stories.js
+++ src/components/Input/input.stories.js
@@ -1,12 +1,13 @@
-import Icon from "components/Icon";
 import React from "react";
 import Input from ".";
+import Icon from "components/Icon";
+
 import { ReactComponent as ClipIcon } from "assets/icon/clip.svg";
 import { ReactComponent as SmileIcon } from "assets/icon/smile.svg";
 
 export default {
-  title: "UI 组件/Input",
-  component: Input
+    title: "UI 组件/Input",
+    component: Input,
 };
 
 export const Default = () => <Input />;
@@ -14,8 +15,11 @@ export const Default = () => <Input />;
 export const Search = () => <Input.Search />;
 
 export const WithAffix = () => (
-  <Input
-    prefix={<Icon icon={ClipIcon} color="#cccccc" />}
-    suffix={<Icon icon={SmileIcon} color="#cccccc" />}
-  />
+    <Input
+        prefix={<Icon icon={ClipIcon} color="#cccccc" />}
+        suffix={<Icon icon={SmileIcon} color="#cccccc" />}
+    />
 );
+
+export const InputTextWithLabel = () => <Input.Text label="昵称" />;
+export const InputTextWithoutLabel = () => <Input.Text />;
